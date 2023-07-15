a54add6c3cb694407e636d5b9dbb7110cbf3c6a3
Faiem-qy <f.quanyoung@gmail.com>
2023-07-14 23:59:47 -0400

    implement eqarrays function

---
+++ eqArrays.js
@@ -0,0 +1,62 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  // if the values match, print the following: Assertion Passed: [actual] === [expected] (but with the values filled in);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+    // Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+  } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  // if the values match, print the following: Assertion Passed: [actual] === [expected] (but with the values filled in);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+    // Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+  } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {// if array is not equal return false
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    // loop over arrays and then compare the values, return false if not equal
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  // return true is equal
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
