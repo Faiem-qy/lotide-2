eb68fa6e09b9726ab8994df9617be858d013de65
Faiem-qy <f.quanyoung@gmail.com>
2023-07-19 23:22:53 -0400

    implement countLetters function

--- countLetters.js
+++ countLetters.js
@@ -17,3 +17,22 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
 
+
+
+// Take a sentence as a string and then return the count of each of the letters in that sentence
+const countLetters = function(inputString) {
+  // declare result and return result
+  let result = {};
+  //  for of loop to loop over letters
+  for (const letter of inputString) {
+    if (result[letter]) {
+      // if there is a letter add
+      result[letter] += 1;
+       } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("LHL"));
