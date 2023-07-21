392afccb262e581ce5853a6060b25b86cca03561
Faiem-qy <f.quanyoung@gmail.com>
2023-07-20 22:52:54 -0400

    made letterPositions file and wrote Pseudocode

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(sentence) {
+  const results = {};
+
+  //convert all letters to lowercase and split it into an array of characters
+
+  // Iterate through each character in the sentence
+  // if space skip this character
+  // if result already contains letter, Add 
+  // else create new key in result with an array containing the current index i
+
+  return results;
+};
+
+letterPositions("lighthouse in the house");
