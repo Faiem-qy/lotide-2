b042584df23e2e55a91e7a0af03d47556943a27f
Faiem-qy <f.quanyoung@gmail.com>
2023-07-20 22:59:11 -0400

    implemented letterositions function

--- letterPositions.js
+++ letterPositions.js
@@ -2,13 +2,24 @@ const letterPositions = function(sentence) {
   const results = {};
 
   //convert all letters to lowercase and split it into an array of characters
+  const letters = sentence.toLowerCase().split('');
 
   // Iterate through each character in the sentence
-  // if space skip this character
-  // if result already contains letter, Add 
-  // else create new key in result with an array containing the current index i
+  for (let i = 0; i < letters.length; i++) {
+    const letter = letters[i];
+    // if space skip this character
+    if (letter === ' ') {
+      continue;
+      // if result already contains letter, Add to current index i
+    } else if (results[letter]) {
+      results[letter].push(i);
+      // else create new key in result with an array containing the current index i
+    } else {
+      results[letter] = [i];
+    }
+  }
 
   return results;
 };
 
-letterPositions("lighthouse in the house");
+console.log(letterPositions("lighthouse in the house"));
