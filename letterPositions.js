d3ddfd4fb15a564c86d7df1917b2752215e24152
Faiem-qy <f.quanyoung@gmail.com>
2023-07-22 22:58:50 -0400

    completed letterPositions.js, made new file findKeyByValue

--- letterPositions.js
+++ letterPositions.js
@@ -23,3 +23,59 @@ const letterPositions = function(sentence) {
 };
 
 console.log(letterPositions("lighthouse in the house"));
+
+const assertEqual = function(actual, expected) {
+
+  // if the values match, print the following: Assertion Passed: [actual] === [expected] (but with the values filled in);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+    // Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+  } else {
+    (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {// if array is not equal return false
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    // loop over arrays and then compare the values, return false if not equal
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  // return true if equal
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  // use eqArrays to compare the arrays
+  if (eqArrays(arr1, arr2)) {
+    // if the arrays are equal
+    console.log(`✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    // if the arrays are not equal
+    console.log(`🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+// Test for 'hello' string
+const result = letterPositions("hello")
+
+const expected = {
+  h: [0],
+  e: [1],
+  l: [2, 3],
+  o: [4]
+}
+
+assertArraysEqual(result.h, expected.h);
+assertArraysEqual(result.e, expected.e);
+assertArraysEqual(result.l, expected.l);
+assertArraysEqual(result.o, expected.o);
+
+assertArraysEqual(letterPositions("hello").e, [1]);
