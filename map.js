7f3ce95ffee85151dee477c6a3b070eac720b8c2
Faiem-qy <f.quanyoung@gmail.com>
2023-07-24 21:49:44 -0400

    made file map.js

---
+++ map.js
@@ -0,0 +1,15 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+// const results1 = words.map(word => word[0]);
+// console.log(results1);
+
+// const results1 = map(words, word => word[0]);
+// console.log(results1);
+
+const map = function() {
+  const results = [];
+
+  return results;
+};
+
+console.log(map);
