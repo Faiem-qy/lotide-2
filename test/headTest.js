31cbd96f8d4b1078c64f26e6a2339372f22ab940
Faiem-qy <f.quanyoung@gmail.com>
2023-08-04 23:20:31 -0400

    removed commented test code

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,5 @@
 const head = require('../head')
-// const assertEqual = require('../assertEqual')
-const assert = require('chai').assert;
+\const assert = require('chai').assert;
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
@@ -11,9 +10,4 @@ describe("#head", () => {
   });
 });
 
-
-
-
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
