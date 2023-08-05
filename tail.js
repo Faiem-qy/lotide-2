181698426a12194fb1f6833519e3fe31e06b2c6e
Faiem-qy <f.quanyoung@gmail.com>
2023-08-04 21:45:50 -0400

    exported tail function and required the functions (assertEqual & tail) in tailTest file

--- tail.js
+++ tail.js
@@ -1,19 +1,24 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = require('./assertEqual');
 
-  // if the values match, print the following: Assertion Passed: [actual] === [expected] (but with the values filled in);
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 
-    // Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
-  } else {
-    (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
-  }
-};
+// const assertEqual = function(actual, expected) {
+
+//   // if the values match, print the following: Assertion Passed: [actual] === [expected] (but with the values filled in);
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+//     // Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+//   } else {
+//     (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+//   }
+// };
 
 const tail = function(arr) {
   //make new array omitting the first element
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+
+
+module.exports = tail;
